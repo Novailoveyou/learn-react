7a44974366c9a2f21ece82ecfdc16bdec9850a79
novailoveyou <novailoveyoux@gmail.com>
2021-04-15 01:50:34 +0300

    JSX & Fragments

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import React, { Component } from 'react'
+import React, { Fragment, Component } from 'react'
 import './App.css'
 
 class App extends Component {
   render() {
     return (
-      <div className='App'>
+      <Fragment>
         <h1>Hello from React</h1>
-      </div>
+      </Fragment>
     )
+    // return React.createElement(
+    //   'div',
+    //   { className: 'App' },
+    //   React.createElement('h1', null, 'Hello from React')
+    // )
   }
 }
 
