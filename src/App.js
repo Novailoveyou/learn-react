cbb24e4693816b49664239d3fb2c23c04a056ef3
novailoveyou <novailoveyoux@gmail.com>
2021-04-17 12:47:35 +0300

    Components, Props & PropTypes, Started with Component State

--- src/App.js
+++ src/App.js
@@ -1,34 +1,16 @@
 import React, { Fragment, Component } from 'react'
+import Navbar from './components/layout/Navbar'
+import UserItem from './components/users/UserItem'
 import './App.css'
 
 class App extends Component {
-  foo2 = () => 'Bars'
-
   render() {
-    const name = 'John Doe'
-    const loading = false
-    const showName = true
-
-    // if (loading) {
-    //   return <h4>Loading...</h4>
-    // }
-
-    const foo = () => 'Bar'
-
     return (
-      <Fragment>
-        {loading ? (
-          <h4>Loading...</h4>
-        ) : (
-          <h1>Hello {showName && this.foo2()}</h1>
-        )}
-      </Fragment>
+      <div className='App'>
+        <Navbar />
+        <UserItem />
+      </div>
     )
-    // return React.createElement(
-    //   'div',
-    //   { className: 'App' },
-    //   React.createElement('h1', null, 'Hello from React')
-    // )
   }
 }
 
