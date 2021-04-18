ca1422502615b0bddd87b41f35ce8243aa179b90
novailoveyou <novailoveyoux@gmail.com>
2021-04-18 14:22:38 +0300

    Spinner Component & Refactoring

---
+++ src/components/layout/Spinner.js
@@ -0,0 +1,11 @@
+import spinner from './spinner.gif'
+
+const Spinner = () => (
+  <img
+    src={spinner}
+    alt='Loading...'
+    style={{ display: 'block', width: '200px', margin: 'auto' }}
+  />
+)
+
+export default Spinner
