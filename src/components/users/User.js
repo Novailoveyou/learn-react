e34171d1f1f6f31b9c1353e8f714f4a35fbf2102
novailoveyou <novailoveyoux@gmail.com>
2021-05-02 22:33:27 +0300

    moving User state to context

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,10 +1,15 @@
-import { useEffect } from 'react'
+import { useEffect, useContext } from 'react'
 import Spinner from '../layout/Spinner'
 import Repos from '../repos/Repos'
 import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
+import GithubContext from '../../context/github/githubContext'
 
-const User = ({ user, loading, getUser, getUserRepos, repos, match }) => {
+const User = ({ getUserRepos, repos, match }) => {
+	const githubContext = useContext(GithubContext)
+
+	const { getUser, loading, user } = githubContext
+
 	useEffect(() => {
 		getUser(match.params.login)
 		getUserRepos(match.params.login)
